cae296382b7f4e4097123a1e02dd75c9b4341007
Cailum Leyshon <cailumleyshon@gmail.com>
2023-09-15 15:11:31 +0100

    Modify loop.js

--- loop.js
+++ loop.js
@@ -1,15 +1,12 @@
 function getRating() {
   let rating = prompt("What star rating would you give this page from 1-5");
 
-  if (rating <= 5 && rating > 0) {
-    for (i = 0; i < rating; i++) {
-      document.write("<img id='croissant' src='croissant.png'/>");
-    }
-  } else if (rating > 5) {
-    alert("I said 1-5.. but thanks for the higher number ;)");
-    getRating();
-  } else if (rating == 0) {
-    alert("That's not very nice.. :( please rate fairly");
-    getRating();
+  while (!(rating > 0 && rating <= 5)) {
+    alert("Please enter a rating between 1 and 5.");
+    rating = prompt("What star rating would you give this page from 1-5");
+  }
+
+  for (let i = 0; i < rating; i++) {
+    document.write("<img id='croissant' src='croissant.png'/>");
   }
 }
